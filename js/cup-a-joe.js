2873bc09eafe4e880d7cfec2c8505d704b6ac0bb
Camden_Shaw <camden.shaw@gmail.com>
2017-07-21 13:53:45 -0700

    commit 4.0.1

--- js/cup-a-joe.js
+++ js/cup-a-joe.js
@@ -1,1 +1,1 @@
-alert('java script is totally working');// set background to 'colour' to know that javascript is working
+alert('java script is kinda working');// set background to 'colour' to know that javascript is working
