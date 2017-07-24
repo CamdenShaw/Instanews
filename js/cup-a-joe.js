d93dff8d31291112d9d37fbe35f0e408a14ff2c5
Camden_Shaw <camden.shaw@gmail.com>
2017-07-23 23:14:46 -0700

    Json is now 100% working.  Now to move onto the design aspect...

--- js/cup-a-joe.js
+++ js/cup-a-joe.js
@@ -2,7 +2,7 @@
 
 var menu = $('.selector');
 var aPIKey = 'ea94cfe26458489e895da92d99390de0';
-var artTitle, artItems, artCapt, artUrl, author, director, artList, $artList = $('.articles');
+var artTitle, artItems, artCapt, artUrl, author, director, artList, artDescript, $artList = $('.articles');
 var nYTUrl, arrImg = '';
 var artImg = [];
 
@@ -25,15 +25,6 @@ menu.change( function(event) {
 
     //console.log(nYTUrl);
   }// close books if statement
-
-  else if (selected === 'community') {
-    alert( 'community has been clicked and listened too');
-    nYTUrl = 'http://api.nytimes.com/svc/community/v3/user-content/recent.json';
-    nYTUrl += '?' + $.param({
-      'api-key': aPIKey
-    });  // close parameters declaration
-    //console.log(nYTUrl);
-  } //close community if statement
 
   else if (selected === 'movies') {
     alert( 'movies has been clicked and listened too');
@@ -62,32 +53,78 @@ menu.change( function(event) {
   
   .done(function(data){
     var artData = '';
+    artImg = '';
     var filterData = '';
     var artData2 = [];
     var i=0;
-    artData2 = [];
 
     artData = data.results;
-    //console.log(filterData);
+    //console.log(artData);
 
     // function filterItem (item){
     //     return item.toString().length > 0;
     //   }
 
     $.each(artData, function(key, value){
+    if(selected != 'books' && selected != 'movies' && selected != '--top stories--' && selected != '--sections--'){
 
-      artImg = value.multimedia[4].url;
+      artImg = value.multimedia;
 
-      if (!!artImg && typeof artImg === 'string'){
+      console.log(artImg.length);
+
+      if (artImg.length > 1){
         artData2.push(data.results[i]);
         artData = artData2.slice(0, 12);
         i++;
       }
+      else {
+        i++;
+      }
       console.log('count', i, 'art', artData, 'art2', artData2);
 
-        //console.log(artData2);
+      console.log('art',artData);
+    } // close set top stories variables IF statement
 
-        if (selected === 'home' || selected !== 'arts' || selected !== 'business' || selected === 'sports' || selected === 'technology' || selected === 'fashion') {
+    else if (selected === 'books'){
+
+      artDescript = value.description;
+
+      //console.log('length', artDescript.length);
+
+      if (!!artDescript && typeof artDescript != undefined){
+        artData2.push(data.results[i]);
+        artData = artData2.slice(0, 12);
+        i++;
+      }
+      else {
+        i++;
+      }
+      console.log('count', i, '/nart', artData, '/nart2', artData2);
+    } //  close set books variables IF statement
+
+    else if (selected === 'movies'){
+
+      artImg = value.multimedia;
+
+      //console.log(artImg.length);
+
+      if (!!artImg && typeof artImg === 'object'){
+        artData2.push(data.results[i]);
+        artData = artData2.slice(0, 12);
+        i++;
+      }
+      else {
+        i++;
+      }
+      //console.log('count', i, '/nart', artData, '/nart2', artData2);
+
+      //console.log('art',artData);
+    } //  close set movies variables IF statement
+    
+    })
+    $.each(artData, function(kay, value){
+        if (selected != 'books' && selected != 'movies' && selected != '--top stories--' && selected != '--sections--') {
+
           // var array = value.multimedia;
           // arrImg = array.slice(0,1);
           // console.log(arrImg);
@@ -104,9 +141,6 @@ menu.change( function(event) {
 
           //console.log(artItems);
         } //  close top stories else statement
-      else if (selected === 'community') {
-
-      } //  community
 
       else if (selected === 'movies') {
         artImg = '<img src="' + value.multimedia.src + '"/>';
@@ -116,26 +150,21 @@ menu.change( function(event) {
         artCapt = '<p>' + value.summary_short + '"</p>';
         //console.log(artCapt);
 
-        artItems.push('<li>' + artUrl + artCapt + '</li>');
+        artItems += '<li>' + artUrl + artCapt + '</li>';
 
         //console.log(artItems);
       } // close movies if statement
 
       else if (selected === 'books') {
-        var author = '<p>"' + value.author + '"</p>';
+        var author = '<h3>"' + value.author + '"</h3>';
         var arrUrl = '';
         arrUrl = value.sunday_review_link;
-        if (arrUrl !== 0 || arrUrl !== null || arrUrl !=='' || arrUrl !== 'undefined') {
           artUrl = '<a href="' + arrUrl + '" target="_blank">';
           console.log(arrUrl);
-          artTitle = '<p>' + artUrl + value.title + '</a></p>';
+          artTitle = '<h2>' + artUrl + value.title + '</a></h2>';
           artCapt = '<p>' + value.description + '</p>';
           artItems += '<li>' + artTitle + author + artCapt + '</li>';
           //console.log(artItems);
-        }
-        else {
-          arrUrl = '';
-        }
 
       } //  close book reviews if statement
       else {
